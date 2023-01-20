562d2890c9eaf562edd7da6e64ea0907d76301de
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-20 08:14:07 -0500

    added requirements for employment libraries

--- index.js
+++ index.js
@@ -2,25 +2,73 @@ const inquirer = require('inquirer');
 const generateSite = require('utils/generateSite.js');
 const fs = require('fs');
 
+const Manager = require('lib/Manager.js');
+const Engineer = require('lib/Engineer.js');
+const Intern = require('lib/Intern.js');
+
 const addManager = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is the name of the manager?'
     },
     {
         type: 'input',
         name: 'employeeID',
-        message: 'What is your employee ID number?'
+        message: 'What is their employee ID number?'
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?'
+        message: 'What is their email address?'
     },
     {
         type: 'input',
         name: 'officeNumber',
-        message: 'What is your office number?'
+        message: 'What is their office number?'
+    },
+]
+const addEngineer = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the engineer?'
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'What is their employee ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is their office number?'
+    },
+]
+const addIntern = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the intern?'
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'What is their employee ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is their office number?'
     },
 ]

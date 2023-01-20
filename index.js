f760853f886de2ff594c89b51e1f0817fcc5e907
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-20 08:43:18 -0500

    attempting to get questions to stop if newEmp answer is no

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer'); 
-const generateSite = require('utils/generateSite.js');
+// const generateSite = require('utils/generateSite.js');
 const fs = require('fs');
 
-const Manager = require('lib/Manager.js');
-const Engineer = require('lib/Engineer.js');
-const Intern = require('lib/Intern.js');
+// const Manager = require('lib/Manager.js');
+// const Engineer = require('lib/Engineer.js');
+// const Intern = require('lib/Intern.js');
 
 const empType = [
     {
@@ -15,7 +15,8 @@ const empType = [
     {
         type: 'list',
         name: 'empType',
-        message: 'Is this person a manager or an engineer?'
+        message: 'Is this person a manager or an engineer?',
+        choices: ['Manager', 'Engineer']
     },
 ]
 
@@ -87,3 +88,15 @@ const addIntern = [
         message: 'What is their office number?'
     },
 ]
+
+function init() {
+    inquirer.prompt(empType).then((answers) => {
+        if (answers === 'No') {
+            console.log('This field is only for NEW employees')
+        } else {
+            //continue to next question
+        };
+    })
+}
+
+init();

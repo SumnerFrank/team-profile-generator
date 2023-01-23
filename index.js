1363f8f68d97366c7ce0177f5541eeb6997cfa02
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-23 08:59:40 -0500

    notated file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//Dependencies
 const inquirer = require('inquirer'); 
 const generateSite = require('utils/generateSite.js');
 const fs = require('fs');
 
+//Employee libraries
 const Manager = require('lib/Manager.js');
 const Engineer = require('lib/Engineer.js');
 const Intern = require('lib/Intern.js');
 
+//prompts to add data to libraries
 const addEmp = [
     {
         type: 'confirm',

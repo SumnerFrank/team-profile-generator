a3da1d56970a7cfef9e90e767ac1900529e371b4
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-23 13:52:09 -0500

    updated questions for prompt

--- index.js
+++ index.js
@@ -11,9 +11,43 @@ const Intern = require('lib/Intern.js');
 //prompts to add data to libraries
 const addEmp = [
     {
+        type: 'list',
+        name: 'role',
+        message: 'To add an employee, choose a role listed below:',
+        choices: ['Intern', 'Engineer', 'No additional team members']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the employee's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter the ID number of the employee.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the employee's email address."
+    }, 
+    // for Engineers only
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter the employee's GitHub username."
+    },
+    // for Interns only
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter the name of the school the intern attends'
+    },
+    //finish or continue
+    {
         type: 'confirm',
-        name: 'newEmp',
-        message: 'Is this person a new employee?',
+        name: 'AnotherEmp',
+        message: 'Would you like to add another employee?'
     }
 ]
 
